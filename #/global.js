58e971d60a44674e61b155a7281d9b9d4bcec325
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2023-12-27 14:37:26 +0700

    Create global.js

---
+++ #/global.js
@@ -0,0 +1,12 @@
+
+
+var allowedDomains = ['bstlar.com', 'linkvertise.com'];
+var currentDomain = window.location.hostname;
+
+if (allowedDomains.includes(currentDomain)) {
+  document.getElementById("downloadContent").style.display = "flex";
+  document.getElementById("mainContent").style.display = "none";
+} else {
+  document.getElementById("downloadContent").style.display = "none";
+  document.getElementById("mainContent").style.display = "flex";
+}
